52ef0100f9421e834e3ad0d6ee1be4f6994b309d
udayshinde-259 <shindeuday524@gmail.com>
2026-09-06 15:58:02 +0530

    Update on font and done design of project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { Navbar } from "@/components/ui/navbar";
-import { SignOutButton } from "@clerk/nextjs";
+import { SignOutButton, useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react"
 
 export default function(){
+    const {user} = useUser();
     let [msg, setmsg] = useState<string>("");
+    
 
     useEffect(()=>{
         async function getData(){
@@ -18,5 +20,14 @@ export default function(){
     }, [])
     return <div>
         <Navbar/>
+        <div>
+            <div className="mb-6 sm:mb-8">
+                <p className=" text-2xl sm:text-3xl font-bold text-gray-900 mb-2">Welcome Back, {user?.firstName}</p>
+                <p className="text-gray-600">Here's all the board you can work on</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
     </div>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,24 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { Google_Sans, Geist } from "next/font/google";
+import { Google_Sans } from "next/font/google";
+
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
-const googleSans = Google_Sans({
-  variable: "--font-google-sans",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Flowbard",
   description: "A Trello clone built with Next.js",
 };
 
-export default function RootLayout({ children }:LayoutProps<"/">) {
+const googleSans = Google_Sans({
+  variable: "--font-google-sans",
+  subsets: ["latin"],
+  adjustFontFallback: false
+});
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", googleSans.variable, "font-sans", geist.variable)}
+      className={`h-full antialiased ${googleSans.variable} font-sans`}
     >
       <body className="min-h-full flex flex-col">
         <ClerkProvider>{children}</ClerkProvider>
